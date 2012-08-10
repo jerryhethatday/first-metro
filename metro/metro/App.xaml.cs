39b4051288149a4f0cbffab56daa84379d2fd184
Jerry <jerryhe@jerryhe.com>
2012-08-10 16:26:37 +0800

    我的第一个metro app
    今天终于完整的写了一个Metro风格的APP,通过教程（教程一和教程二）基本上把他的工作机理搞通了，明天继续加油弄。

---
+++ metro/metro/App.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace metro
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected async override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            // Do not repeat app initialization when already running, just ensure that
+            // the window is active
+            if (args.PreviousExecutionState == ApplicationExecutionState.Running)
+            {//如果应用已经在运行，则仅仅激活当前窗口
+                Window.Current.Activate();
+                return;
+            }
+
+           
+
+            // Create a Frame to act navigation context and navigate to the first page
+            var rootFrame = new Frame();
+            metro.Common.SuspensionManager.RegisterFrame(rootFrame, "appFrame");//注册一个frame
+            if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+            {
+                //TODO: Load state from previously suspended application
+                await metro.Common.SuspensionManager.RestoreAsync();
+            }
+            if (rootFrame.Content == null)//因此仅当导航状态未被还原时才修改此代码来调用 Frame.Navigate。
+            {
+                if (!rootFrame.Navigate(typeof(MainPage)))
+                {
+                    throw new Exception("Failed to create initial page");
+                }//最后，代码会调用 Frame.Navigate 方法来转至 MainPage，并且激活窗口。由于将添加代码来还原应用的导航状态，
+            }
+            // Place the frame in the current Window and ensure that it is active
+            Window.Current.Content = rootFrame;
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private async  void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+           await metro.Common.SuspensionManager.SaveAsync();
+            deferral.Complete();
+        }
+    }
+}
